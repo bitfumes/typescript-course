a60adf8f0681926b317dd22e4d7a1dda1dcb7f44
sarthaksavvy <bitfumes@gmail.com>
2021-12-28 10:58:17 +0530

    02 TypeScript Course - TypeScript Compiler

--- index.ts
+++ index.ts
@@ -1,11 +1,5 @@
-// let name = "Sarthak";
-// name = 1;
-// console.log(typeof name);
-// name = false;
-// console.log(typeof name);
-
 function sum(a: number, b: number): number {
   return a + b;
 }
 
-console.log(sum("1", false));
+console.log(sum(1, 2));
