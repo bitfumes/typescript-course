e159b49e795f0382c1f7d0af25874b479de2602c
sarthaksavvy <bitfumes@gmail.com>
2021-12-29 11:31:25 +0530

    04 TypeScript Course - type Inference

---
+++ 04/index.ts
@@ -0,0 +1,13 @@
+let guests = ['Joe', 'Jeni', 'John', 22, true]
+console.log(typeof guests);
+
+
+// let myName = "Sarthak"
+// myName = 2022
+// console.log(typeof myName);
+
+// let year
+// year = 2022
+// year = "abcd"
+// console.log(typeof year);
+// type Inference
