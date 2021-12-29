557118def67337b842cb79f34f28f49ae3a96dab
sarthaksavvy <bitfumes@gmail.com>
2021-12-29 11:22:32 +0530

    03 TypeScript Course - define types

---
+++ 03/index.ts
@@ -0,0 +1,24 @@
+// PRIMITIVE TYPES
+// string
+let myName: string = "Sarthak"
+// number
+let year: number = 2022
+// boolean
+let isEnable: boolean
+isEnable = true
+// null
+// undefined
+// symbol
+
+// OBJECT TYPES
+// arrays, objects, clases
+
+// Types for Array
+let guests: string[] = ['Joe', 'Jeni', 'John']
+
+// Types for a function
+// let killBill: (name: string) => string;
+
+const killBill = function (name: string): string {
+    return name.toUpperCase()
+}
