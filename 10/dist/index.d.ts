d4f55190a992942224fca028764c8e44cb5a83b8
sarthaksavvy <bitfumes@gmail.com>
2022-01-02 07:42:01 +0530

    10 TypeScript Course - undefined vs null

---
+++ 10/dist/index.d.ts
@@ -0,0 +1,1 @@
+declare let price3: number | undefined;
---
+++ 10/dist/index.js
@@ -0,0 +1,30 @@
+"use strict";
+// null & undefined
+// let price: number;
+// price = 10
+// console.log(typeof price);
+// price = undefined
+// console.log(typeof price);
+// let price = null;
+// console.log(price);
+// // price = null
+// console.log(price);
+// let price: undefined;
+// console.log(typeof price, price);
+// let price2: null;
+// console.log(typeof price2, price2);
+// price2 = null
+// console.log(typeof price2, price2);
+// let price3: number | undefined | null;
+// console.log(price3);
+// // console.log(price3 == null)
+// // // console.log(price3 === null)
+// console.log(price3 == undefined)
+// console.log(price3 === undefined)
+let price3;
+price3 = null;
+console.log(price3);
+// console.log(price3 == null) // true
+// console.log(price3 === null) // true
+console.log(price3 == undefined); // true
+console.log(price3 === undefined); // false
